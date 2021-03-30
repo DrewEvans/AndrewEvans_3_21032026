bc6db0f79065ba64a1ca4586861e2a2ac5d79dfd
Andrew E <45772398+DrewEvans@users.noreply.github.com>
2021-03-30 19:14:52 +0200

    comments added in main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-const loadingScreen = document.querySelector('.loading-wrapper');
-
+//trigger event when window is loaded 
 window.addEventListener('load', () => {
+    // select class loading wrapper from html
+    const loadingScreen = document.querySelector('.loading-wrapper');
+    //once loaded change .loading-wrappers orginal css style with .loading-finshed style 
     loadingScreen.classList.add('loading-finished')
 });
